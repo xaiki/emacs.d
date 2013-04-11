b4bd98323b46c6b5d81899cca3b6b8e6447f8b30
Niv Sardi <xaiki@evilgiggle.com>
2013-04-11 18:09:46 -0300

    profesional cal
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(org-agenda-files (quote ("~/Documents/org/professional-cal.org")))
  '(org-clock-persist-query-save t)
  '(org-clock-sound t)
  '(org-show-notification-handler (quote notifications-notify))
