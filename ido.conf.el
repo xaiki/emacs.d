b74e287d2a8f3840c3045f298f10f0458aa7e07c
Niv Sardi <xaiki@evilgiggle.com>
2013-11-07 21:45:48 -0300

    IDO: case insensitive

--- ido.conf.el
+++ ido.conf.el
@@ -1,5 +1,6 @@
 (setq ido-default-buffer-method 'selected-window
       ido-default-file-method 'selected-window
-      ido-enable-flex-matching t) ; case insensitive matching
+      ido-enable-flex-matching t
+      ido-case-fold t) ; case insensitive matching
 (setq ido-use-filename-at-point 'guess)
 (ido-mode 1)                            ; Interactively Do Things
