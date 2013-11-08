4ed3f963d9010f8329db24a1c3ae8e02a5e3c6bc
Niv Sardi <xaiki@evilgiggle.com>
2013-11-07 21:30:24 -0300

    kill some semantic, I never use it anyway

--- semantic.conf.el
+++ semantic.conf.el
@@ -1,12 +1,3 @@
-(eval-after-load "semantic/db"
-  '(add-to-list 'semanticdb-project-root-functions
-		(defun jd:semanticdb-is-project-root (directory-name)
-		  (save-match-data
-		    (when (string-match
-			   (concat "^\\(" (regexp-quote (expand-file-name jd:projects-directory)) "/[^/]+\\)")
-			   directory-name)
-		      (match-string 1 directory-name))))))
-
 (setq semantic-idle-summary-function
       (defun jd:semantic-format-tag-concise-or-full (tag &optional parent color)
         "Return the full prototype if we have enough size in the echo area to print it.
