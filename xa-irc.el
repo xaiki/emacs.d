4ab9d38302007f6c73122b03c53b0ab6f8f338c2
Niv Sardi <xaiki@evilgiggle.com>
2014-10-14 18:56:29 -0300

    fix for znc irc bouncer
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- xa-irc.el
+++ xa-irc.el
@@ -1,13 +1,38 @@
+(defun* erc-znc (&rest spec
+                       &key server port nick network
+                       &allow-other-keys)
+  "hack arround erc-tlc"
+  (let* ((server  (plist-get spec :server))
+          (port    (plist-get spec :port))
+          (nick    (plist-get spec :nick))
+          (network (plist-get spec :network))
+          (secret  (let ((found (nth 0 (auth-source-search
+                                        :host server
+                                        :user (concat nick "#" network)
+                                        :require '(:user :secret)
+                                        ))))
+                     (let ((secret (plist-get found :secret)))
+                       (if (functionp secret)
+                           (funcall secret)
+                         secret)))))
+       (erc-tls
+        :server   server
+        :port     port
+        :nick     (concat nick "/" network)
+        :password (concat nick "/" network ":" secret))))
+
 (defun xa:erc-start ()
-  (erc-tls
+  (setq erc-prompt-for-password nil)
+  (erc-znc
    :server "core.evilgiggle.com"
    :port 7778
-   :nick "xaiki:freenode")
-  (erc-tls
+   :nick "xaiki"
+   :network "freenode")
+  (erc-znc
    :server "core.evilgiggle.com"
    :port 7778
-   :nick "xaiki:oftc")
-  )
+   :nick "xaiki"
+   :network "oftc"))
 
 (defun xa:erc-stop ()
   "Disconnect from IRC servers."
