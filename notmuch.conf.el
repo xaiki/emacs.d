ca702d2b9b1616f9f10b5fe3e83f73ccce8f1cbc
Niv Sardi <xaiki@evilgiggle.com>
2014-10-14 18:56:14 -0300

    notmuch update
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- notmuch.conf.el
+++ notmuch.conf.el
@@ -1,7 +1,4 @@
 (setq notmuch-search-oldest-first nil)
-(require 'notmuch-address)
-(setq notmuch-address-command "~/bin/notmuch-goobook.sh")
-(notmuch-address-message-insinuate)
 
 (defun xa:notmuch-address-selection-function (prompt collection initial-input)
   "Call (`completing-read'
@@ -16,7 +13,7 @@
 (defun xa:completion-at-point (prompt collection initial-input)
   (completion-at-point))
 
-(setq notmuch-address-selection-function 'xa:notmuch-address-selection-function)
+;; (setq notmuch-address-selection-function 'xa:notmuch-address-selection-function)
 ;;(setq notmuch-address-selection-function 'xa:complete-to)
 ;;(setq notmuch-address-selection-function 'xa:completion-at-point)
 
@@ -52,3 +49,6 @@
 (define-key notmuch-hello-mode-map "a"
   (expose #'notmuch-hello-search "tag:archive"))
 
+(require 'notmuch-address)
+(setq notmuch-address-command "~/bin/notmuch-contact.py")
+(notmuch-address-message-insinuate)
