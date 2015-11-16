f67a137dd059f86e944be721735317f4f57ff2e1
Niv Sardi <xaiki@evilgiggle.com>
2015-11-16 16:51:57 -0300

    big update... YAT
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ markdown-mode.conf.el
@@ -0,0 +1,36 @@
+(require 'mmm-mode)
+(setq mmm-global-mode 'maybe)
+
+(defun my-mmm-markdown-auto-class (lang &optional submode)
+  "Define a mmm-mode class for LANG in `markdown-mode' using SUBMODE.
+If SUBMODE is not provided, use `LANG-mode' by default."
+  (let ((class (intern (concat "markdown-" lang)))
+        (submode (or submode (intern (concat lang "-mode"))))
+        (front (concat "^```" lang "[\n\r]+"))
+        (back "^```"))
+    (mmm-add-classes (list (list class :submode submode :front front :back back)))
+    (mmm-add-mode-ext-class 'markdown-mode nil class)))
+
+;; Mode names that derive directly from the language name
+(mapc 'my-mmm-markdown-auto-class
+      '("awk" "bibtex" "c" "cpp" "css" "html" "latex" "lisp" "makefile"
+        "markdown" "python" "r" "ruby" "sql" "stata" "xml"))
+
+(mmm-add-classes
+ '((markdown-js
+    :creation-hook (lambda () (web-mode-set-content-type "jsx"))
+    :submode web-mode
+    :front "^```js[\n\r]+"
+    :back "^```$")))
+
+(mmm-add-mode-ext-class 'markdown-mode nil 'markdown-js)
+
+(mmm-add-classes
+ '((markdown-shell
+    :submode shell-script-mode
+    :front "^```sh[\n\r]+"
+    :back "^```$")))
+
+(mmm-add-mode-ext-class 'markdown-mode nil 'markdown-shell)
+
+
---
+++ org-clock-save.el
@@ -0,0 +1,2 @@
+;; org-persist.el - furiloche at <2015-11-03 mar 17:02>
+(setq stored-clock-history '(("/home/xaiki/src/Blocktech/TODO.org" . 4448) ("/home/xaiki/src/Blocktech/TODO.org" . 3247) ("/home/xaiki/src/Blocktech/TODO.org" . 1) ("/home/xaiki/src/Blocktech/TODO.org" . 2570) ("/home/xaiki/src/Blocktech/TODO.org" . 792)))
