f0f302bd85f278e6bcca5d2d24e506716179b94f
Niv Sardi <xaiki@evilgiggle.com>
2017-04-03 16:45:53 -0400

    only use authinfo in auth-sources
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- init.el
+++ init.el
@@ -36,9 +36,7 @@
     (eval-after-load (match-string-no-properties 1 file)
       `(load ,(concat user-emacs-directory file)))))
 
-(setq auth-sources '("~/.authinfo.gpg"
-                     "secrets:session"
-                      "secrets:Login"))
+(setq auth-sources '("~/.authinfo.gpg"))
 
 (setq custom-file "~/.emacs.d/custom.el")
 (load custom-file)
