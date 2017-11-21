5257a72cc05933e203d21386a434606723ad0663
Niv Sardi <xaiki@evilgiggle.com>
2017-11-21 00:28:31 -0300

    introduce require-package
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- package.conf.el
+++ package.conf.el
@@ -16,6 +16,16 @@
 (unless package-archive-contents    ;; Refresh the packages descriptions
   (package-refresh-contents))
 
+(defun require-package (package)
+  "Install given PACKAGE if it was not installed before."
+  (if (package-installed-p package)
+      t
+    (progn
+      (unless (assoc package package-archive-contents)
+	(package-refresh-contents))
+      (package-install package)))
+  (require package))
+
 (dolist (package '(web-mode
 ;;                   nm
 ;;                   notmuch
