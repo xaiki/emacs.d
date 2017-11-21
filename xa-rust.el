f90d961af1e265d2ddd77d43b9d890bdbf73c11a
Niv Sardi <xaiki@evilgiggle.com>
2017-11-21 00:28:43 -0300

    import rust config with new require-package
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

---
+++ xa-rust.el
@@ -0,0 +1,23 @@
+;;; xa-rust --- settings for rust-lang
+;;; Code:
+(require-package 'company)
+(require-package 'racer)
+(require-package 'rust-mode)
+(require-package 'flycheck)
+(require-package 'flycheck-rust)
+
+(add-to-list 'auto-mode-alist '("\\.rs\\'" . rust-mode))
+(add-hook 'rust-mode-hook  #'company-mode)
+(add-hook 'rust-mode-hook  #'racer-mode)
+(add-hook 'racer-mode-hook #'eldoc-mode)
+(add-hook 'flycheck-mode-hook #'flycheck-rust-setup)
+(add-hook 'rust-mode-hook
+          '(lambda ()
+	     (setq racer-cmd (concat (getenv "HOME") "/.rust-dev/racer/target/release/racer"))
+	     (setq racer-rust-src-path (concat (getenv "HOME") "/src/rust/src"))
+             (local-set-key (kbd "TAB") #'company-indent-or-complete-common)
+	     (electric-pair-mode 1)))
+
+(provide 'init-rust)
+;;; xa-rust ends here
+
