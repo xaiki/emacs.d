34554f3c90daa99f7d5e037bafa4f3d199f00bf9
Niv Sardi <xaiki@evilgiggle.com>
2018-03-26 19:47:36 -0300

    remove message-font-lock stuff
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- message.conf.el
+++ message.conf.el
@@ -16,50 +16,10 @@
     (setq message-citation-line-function 'message-insert-formatted-citation-line)
     (add-hook 'message-mode-hook 'footnote-mode)
     ;;(setq message-subscribed-address-functions '(gnus-find-subscribed-addresses))
-    
+
     ;;(setq message-signature '(concat "Niv Sardi\n" (nth (random (length xa:message-signatures)) xa:message-signatures)))
-    
+
     (setq message-send-mail-function 'message-smtpmail-send-it)
-    
-    (setq message-font-lock-keywords
-	  (let ((content "[ \t]*\\(.+\\(\n[ \t].*\\)*\\)\n?"))
-	    `((,(message-font-lock-make-header-matcher
-		 (concat "^\\([Tt]o:\\)" content))
-	       (1 'message-header-sub)
-	       (2 'message-header-to nil t))
-	      (,(message-font-lock-make-header-matcher
-		 (concat "^\\(^[GBF]?[Cc][Cc]:\\|^[Rr]eply-[Tt]o:\\)" content))
-	       (1 'message-header-name)
-	       (2 'message-header-cc nil t))
-	      (,(message-font-lock-make-header-matcher
-		 (concat "^\\([Ss]ubject:\\)" content))
-	       (1 'message-header-name)
-	       (2 'message-header-subject nil t))
-	      (,(message-font-lock-make-header-matcher
-		 (concat "^\\([Nn]ewsgroups:\\|Followup-[Tt]o:\\)" content))
-	       (1 'message-header-name)
-       (2 'message-header-newsgroups nil t))
-	      (,(message-font-lock-make-header-matcher
-		 (concat "^\\(X-[A-Za-z0-9-]+:\\|In-Reply-To:\\)" content))
-	       (1 'message-header-name)
-	       (2 'message-header-xheader))
-	      (,(message-font-lock-make-header-matcher
-		 (concat "^\\([A-Z][^: \n\t]+:\\)" content))
-	       (1 'message-header-name)
-       (2 'message-header-other nil t))
-	      ,@(if (and mail-header-separator
-		 (not (equal mail-header-separator "")))
-		    `((,(concat "^\\(" (regexp-quote mail-header-separator) "\\)$")
-		       1 'message-separator))
-		  nil)
-	      ((lambda (limit)
-		 (re-search-forward "\\(>\\(>\\(>\\(>\\(> .+\\)? ?.*\\)? ?.*\\)? ?.*\\)? ?.*\\)" 			    limit t))
-	       (0 'mu4e-cited-1-face)
-	       (1 'mu4e-cited-2-face)
-	       (2 'mu4e-cited-3-face))
-	      ("<#/?\\(multipart\\|part\\|external\\|mml\\|secure\\)[^>]*>"
-	       (0 'message-mml)))))
-    
-    "^ *\\(\\(>+ ?\\)+\\)"
-    "\\([ 	]*\\(\\w\\|[_.]\\)+>+\\|[ 	]*[]>|]\\)+"
+
+
     ))
