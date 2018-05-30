edbfd5deec3f061378b9097c418e4bbf23b50bb7
Niv Sardi <xaiki@evilgiggle.com>
2018-05-30 04:14:55 -0300

    test signoff

--- magit.conf.el
+++ magit.conf.el
@@ -1,6 +1,7 @@
 (require-package 'company)
 (setq magit-repo-dirs `("~/.emacs.d" "~/src"))
-(setq magit-commit-signoff t)
 (setq magit-remote-ref-format 'remote-slash-branch)
 (setq magit-completing-read-function 'magit-ido-completing-read)
 (setq magit-save-some-buffers nil)
+
+(add-hook 'magit-commit-setup-hook '(magit-commit-signoff t))
