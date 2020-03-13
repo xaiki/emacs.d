44c6933ad6a66e051f6cb471a1980013c241238b
Niv Sardi <xaiki@evilgiggle.com>
2020-03-13 12:23:32 -0300

    --amend
    remove many files
    
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- org-clock-save.el
+++
@@ -1,2 +0,0 @@
-;; org-persist.el - jericocoara at <2016-07-18 Mon 11:12>
-(setq stored-clock-history '(("/home/xaiki/src/Endless/endless.org" . 2390) ("/home/xaiki/src/Endless/endless.org" . 1836) ("/home/xaiki/src/Endless/endless.org" . 41016) ("/home/xaiki/src/Endless/endless.org" . 17523) ("/home/xaiki/src/Endless/endless.org" . 17629)))
