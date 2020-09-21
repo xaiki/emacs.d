2f34dfa9d5c7cd7d8be6e42d3841d400cf16f29d
Niv Sardi <xaiki@evilgiggle.com>
2020-09-21 19:53:38 -0300

    remove ibuffer, check-in new custom.el
    Signed-off-by: Niv Sardi <xaiki@evilgiggle.com>

--- custom.el
+++ custom.el
@@ -3,10 +3,25 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(org-clock-persist-query-save t)
+ '(custom-safe-themes
+   '("cc8528fcff6ff85ed132ea83e457a58ae0a49168c93bd752a8c446c61fefcdb5" default))
+ '(org-clock-persist-query-save t t)
  '(org-clock-sound t)
- '(org-show-notification-handler (quote notifications-notify))
- '(safe-local-variable-values (quote ((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook" (add-hook (quote write-contents-functions) (lambda nil (delete-trailing-whitespace) nil)) (require (quote whitespace)) "Sometimes the mode needs to be toggled off and on." (whitespace-mode 0) (whitespace-mode 1)) (whitespace-line-column . 80) (whitespace-style face trailing lines-tail) (require-final-newline . t) (encoding . utf-8)))))
+ '(org-show-notification-handler 'notifications-notify t)
+ '(safe-local-variable-values
+   '((eval ignore-errors "Write-contents-functions is a buffer-local alternative to before-save-hook"
+           (add-hook 'write-contents-functions
+                     (lambda nil
+                       (delete-trailing-whitespace)
+                       nil))
+           (require 'whitespace)
+           "Sometimes the mode needs to be toggled off and on."
+           (whitespace-mode 0)
+           (whitespace-mode 1))
+     (whitespace-line-column . 80)
+     (whitespace-style face trailing lines-tail)
+     (require-final-newline . t)
+     (encoding . utf-8))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
